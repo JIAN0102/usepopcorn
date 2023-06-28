053c31957b2d25baac03b527dd8104a68d50b0a7
Jim Chen <jim.chen@gosaico.com>
2023-06-28 16:23:28 +0800

    [feature]: add Star component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+/*
+ * @Author:Jim Chen
+ * @Date:2023-06-26 13:45:32
+ * @LastEditors:Jim Chen
+ * @LastEditTime:2023-06-28 11:39:08
+ * @Description:
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import StarRating from "./StarRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} size={24} color="red" className="test" />
   </React.StrictMode>
 );
